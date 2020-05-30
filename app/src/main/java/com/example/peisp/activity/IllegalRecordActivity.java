f7142f0e42f12ccce94456a77fb3f4d022c955c9
MHuuuu <nothinghu@163.com>
2020-05-30 14:12:06 +0800

    5.29-界面初稿-1

--- app/src/main/java/com/example/peisp/activity/IllegalRecordActivity.java
+++ app/src/main/java/com/example/peisp/activity/IllegalRecordActivity.java
@@ -1,15 +1,205 @@
 package com.example.peisp.activity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bigkoo.pickerview.builder.TimePickerBuilder;
+import com.bigkoo.pickerview.listener.CustomListener;
+import com.bigkoo.pickerview.listener.OnTimeSelectChangeListener;
+import com.bigkoo.pickerview.listener.OnTimeSelectListener;
+import com.bigkoo.pickerview.view.TimePickerView;
 import com.example.peisp.R;
+import com.example.peisp.adapter.TraceListAdapter;
+import com.example.peisp.model.Trace;
 
-public class IllegalRecordActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class IllegalRecordActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView mIvBack;
+    private ListView mLvTrace;
+    private List<Trace> traceList = new ArrayList<>(10);
+    private Button mBtnDatePicker;
+    private TraceListAdapter adapter;
+
+    private TimePickerView pvCustomTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_illegal_record);
+        initView();
+        initData();
+        initCustomTimePicker();
+
+        mIvBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                intent = new Intent(IllegalRecordActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initView() {
+        mIvBack = (ImageView) findViewById(R.id.iv_back);
+        mLvTrace = (ListView) findViewById(R.id.lv_trace);
+        mBtnDatePicker = (Button) findViewById(R.id.btn_date_picker);
+        mBtnDatePicker.setOnClickListener(this);
+    }
+
+    private void initData() {
+        // 模拟一些假的数据-时间线
+        traceList.add(new Trace("2016-05-25 17:48:00", "[沈阳市] [沈阳和平五部]的派件已签收 感谢使用中通快递,期待再次为您服务!"));
+        traceList.add(new Trace("2016-05-25 14:13:00", "[沈阳市] [沈阳和平五部]的东北大学代理点正在派件 电话:18040xxxxxx 请保持电话畅通、耐心等待"));
+        traceList.add(new Trace("2016-05-25 13:01:04", "[沈阳市] 快件到达 [沈阳和平五部]"));
+        traceList.add(new Trace("2016-05-25 12:19:47", "[沈阳市] 快件离开 [沈阳中转]已发往[沈阳和平五部]"));
+        traceList.add(new Trace("2016-05-25 11:12:44", "[沈阳市] 快件到达 [沈阳中转]"));
+        traceList.add(new Trace("2016-05-24 03:12:12", "[嘉兴市] 快件离开 [杭州中转部]已发往[沈阳中转]"));
+        traceList.add(new Trace("2016-05-23 21:06:46", "[杭州市] 快件到达 [杭州汽运部]"));
+        traceList.add(new Trace("2016-05-23 18:59:41", "[杭州市] 快件离开 [杭州乔司区]已发往[沈阳]"));
+        traceList.add(new Trace("2016-05-23 18:35:32", "[杭州市] [杭州乔司区]的市场部已收件 电话:18358xxxxxx"));
+        adapter = new TraceListAdapter(this, traceList);
+        mLvTrace.setAdapter(adapter);
+    }
+
+    /*
+    private void initTimePicker() {
+        final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar selectedDate = Calendar.getInstance();//系统当前时间
+        Calendar startDate = Calendar.getInstance();//控件起始时间
+        //注：（1）年份可以随便设置 (2)月份是从0开始的（0代表1月 11月代表12月），即设置0代表起始时间从1月开始
+        //(3)日期必须从1开始，因为2月没有30天，设置其他日期时，2月份会从设置日期开始显示导致出现问题
+        startDate.set(1900, 1, 1);//该控件从1900年1月1日开始
+        Calendar endDate = Calendar.getInstance();//控件截止时间
+        endDate.set(2050, 2, 28);//该控件到2050年2月28日结束
+        try {
+            String start1 = "2020-02-28";//格式必须与formatter的格式一致
+            Date date = formatter.parse(start1);
+            selectedDate.setTime(date);//指定控件初始值显示哪一天
+        } catch (Exception e) {
+
+        }
+        //时间选择器
+        TimePickerView pvTime = new TimePickerBuilder(IllegalRecordActivity.this, new OnTimeSelectListener() {
+            public void onTimeSelect(Date date, View v) {
+                String choiceTime = formatter.format(date);//日期 String
+                Toast.makeText(IllegalRecordActivity.this, choiceTime, Toast.LENGTH_LONG).show();
+                long startl = date.getTime();//日期 long
+            }
+        }).setDate(selectedDate)//设置系统时间为当前时间
+                .setRangDate(startDate, endDate)//设置控件日期范围 也可以不设置默认1900年到2100年
+                .setType(new boolean[]{true, true, true, true, true, false})//设置年月日时分秒是否显示 true:显示 false:隐藏
+                .setLabel("年", "月", "日", "时", "分", "秒")
+                .isCenterLabel(false) //是否只显示中间选中项的label文字，false则每项item全部都带有label。
+                .setDividerColor(0xFF24AD9D)//设置分割线颜色
+                .isCyclic(false)//是否循环显示日期 例如滑动到31日自动转到1日 有个问题：不能实现日期和月份联动
+                .build();
+        pvTime.show();
+    }*/
+
+    private void initCustomTimePicker() {
+        /**
+         * @description
+         *
+         * 注意事项：
+         * 1.自定义布局中，id为 optionspicker 或者 timepicker 的布局以及其子控件必须要有，否则会报空指针.
+         * 具体可参考demo 里面的两个自定义layout布局。
+         * 2.因为系统Calendar的月份是从0-11的,所以如果是调用Calendar的set方法来设置时间,月份的范围也要是从0-11
+         * setRangDate方法控制起始终止时间(如果不设置范围，则使用默认时间1900-2100年，此段代码可注释)
+         */
+        Calendar selectedDate = Calendar.getInstance();//系统当前时间
+        Calendar startDate = Calendar.getInstance();
+        startDate.set(2014, 1, 23);
+        Calendar endDate = Calendar.getInstance();
+        endDate.set(2020, 5, 30);
+        //时间选择器 ，自定义布局
+        pvCustomTime = new TimePickerBuilder(this, new OnTimeSelectListener() {
+            @Override
+            public void onTimeSelect(Date date, View v) {//选中事件回调
+                mBtnDatePicker.setText(getTime(date));
+            }
+        })
+                /*.setType(TimePickerView.Type.ALL)//default is all
+                .setCancelText("Cancel")
+                .setSubmitText("Sure")
+                .setContentTextSize(18)
+                .setTitleSize(20)
+                .setTitleText("Title")
+                .setTitleColor(Color.BLACK)
+               /*.setDividerColor(Color.WHITE)//设置分割线的颜色
+                .setTextColorCenter(Color.LTGRAY)//设置选中项的颜色
+                .setLineSpacingMultiplier(1.6f)//设置两横线之间的间隔倍数
+                .setTitleBgColor(Color.DKGRAY)//标题背景颜色 Night mode
+                .setBgColor(Color.BLACK)//滚轮背景颜色 Night mode
+                .setSubmitColor(Color.WHITE)
+                .setCancelColor(Color.WHITE)*/
+                /*.animGravity(Gravity.RIGHT)// default is center*/
+                .setDate(selectedDate)
+                .setRangDate(startDate, endDate)
+                .setLayoutRes(R.layout.pickerview_custom_time, new CustomListener() {
+
+                    @Override
+                    public void customLayout(View v) {
+                        final TextView tvSubmit = (TextView) v.findViewById(R.id.tv_finish);
+                        ImageView ivCancel = (ImageView) v.findViewById(R.id.iv_cancel);
+                        tvSubmit.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                pvCustomTime.returnData();
+                                pvCustomTime.dismiss();
+                            }
+                        });
+                        ivCancel.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                pvCustomTime.dismiss();
+                            }
+                        });
+                    }
+                })
+                .setContentTextSize(18)
+                .setType(new boolean[]{true, true, true,false, false, false})
+                .setLabel("年", "月", "日", "时", "分", "秒")
+                .setLineSpacingMultiplier(1.2f)
+                .setTextXOffset(0, 0, 0, 40, 0, -40)
+                .isCenterLabel(false) //是否只显示中间选中项的label文字，false则每项item全部都带有label。
+                .setDividerColor(0xFF24AD9D)
+                .build();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_date_picker:
+                // pvTime.setDate(Calendar.getInstance());
+                /* pvTime.show(); //show timePicker*/
+                pvCustomTime.show();//弹出时间选择器，传递参数过去，回调的时候则可以绑定此view
+                break;
+        }
+    }
+
+    private String getTime(Date date) {//可根据需要自行截取数据显示
+        Log.d("getTime()", "choice date millis: " + date.getTime());
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        return format.format(date);
     }
 }
---
+++ app/src/main/java/com/example/peisp/model/Worker.java
@@ -0,0 +1,5 @@
+package com.example.peisp.model;
+
+public class Worker {
+    
+}
