4813265acc480a193ed4c3e4c6408c7d997e09a4
ChenY <644093581@qq.com>
2020-05-31 02:05:09 +0800

    处理dialog

--- app/src/main/java/com/example/peisp/activity/IllegalRecordWorkerDetailActivity.java
+++ app/src/main/java/com/example/peisp/activity/IllegalRecordWorkerDetailActivity.java
@@ -3,6 +3,7 @@ package com.example.peisp.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -10,10 +11,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.peisp.R;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
 public class IllegalRecordWorkerDetailActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageView mIvBack;
     private RelativeLayout mBtnCheck;
     private ImageView mIvIllPic;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +46,41 @@ public class IllegalRecordWorkerDetailActivity extends AppCompatActivity impleme
     }
 
     private void initData() {
+        String[] dates = {"场内警报提醒", "接通场内麦克风", "标记为已处理"};
+        adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                dates
+                );
     }
 
 
     @Override
     public void onClick(View view) {
-
+        switch (view.getId()) {
+            case R.id.btn_check:
+                SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.NORMAL_TYPE)
+                    .setTitleText("向文波")
+                    .setContentText("未佩戴安全帽！")
+                    .setConfirmText("发送警报提醒")
+                    .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                        @Override
+                        public void onClick(SweetAlertDialog sDialog) {
+                            // reuse previous dialog instance
+                            sDialog
+                                    .setConfirmClickListener(null)
+                                    .setContentText("警报已发送！")
+                                    .showCancelButton(false)
+                                    .setConfirmText("取消")
+                                    .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+                        }
+                    });
+                pDialog.setCancelable(true);
+                pDialog.showCancelButton(true);
+                pDialog.setCancelText("标记为已处理");
+                pDialog.changeAlertType(SweetAlertDialog.WARNING_TYPE);
+                pDialog.show();
+                break;
+        }
     }
 }
